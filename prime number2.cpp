366f39f76985ea6db12e2ec84422d910e552465e
Dileep7288 <84060027+Dileep7288@users.noreply.github.com>
2021-05-14 20:35:40 +0530

    Add files via upload

---
+++ prime number2.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	cout<<1<<" ";
+	for( int i=2;i<=n/2;i++ )
+	{
+		if(n%i==0)
+		{
+			cout<<i<<" ";
+		}
+	}
+	cout<<n<<" ";
+	return 0;
+}
